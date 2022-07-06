2e4cb30f1a7761b95b03ac7017919b49f2ed6a70
onticinc <onticinc@gmail.com>
2022-07-06 16:50:41 -0600

    made folders

--- data_algo/data.py
+++ data_algo/data.py
@@ -1,3 +1,6 @@
+
+
+
 # 217 Contains Duplicate
 
 
@@ -8,16 +11,17 @@
 List = [1,2,3,4,4,1]
 
 class Solution:
+
+    # Solution 1
     def containsDuplicate(self, nums: List[int]) -> bool:
         
+        return len(set(nums)) != len(nums)
         # Compare the length of array with the length of set of (array)
 		# Converting into set will automatically remove all the duplicates.
 		# That means if the length doesn't match each other, it implies there is duplicate.
-        # T: O(N)
-        # S: O(N)
         
-        return len(set(nums)) != len(nums)
     
+    # Solution 2
     def containsDuplicate(self, nums: List[int]) -> bool:
         
         # Convert list into dictionary then check if there are more than "two" in values of dictionary.
@@ -38,6 +42,7 @@ class Solution:
             
         return False
     
+    # Solution 3
     def containsDuplicate(self, nums: List[int]) -> bool:
         
         # Convert list into dictionary by using Python library
@@ -51,6 +56,9 @@ class Solution:
                 return True
             
         return False
+
+
+    # 
     
     def containsDuplicate(self, nums: List[int]) -> bool:
         
@@ -76,4 +84,9 @@ class Solution:
                 return True
             curr = nums[i]
         return False
+
+
+#   Given an integer array nums and two integers k and t, 
+#       return true if there are two distinct indices i and j in the array 
+#       such that abs(nums[i] - nums[j]) <= t and abs(i - j) <= k  
 
